f02983729291fc02a66c24740a13df951e8fcdc8
Agustin Manelli <agustinmanelli@hotmail.com>
2024-04-09 13:32:22 -0300

    update login page styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,37 +1,41 @@
 "use client";
 import styles from "./login.module.css";
 import { AlinoLogo, ArrowLeft } from "../components/icons";
+import Link from "next/link";
 export default function Login() {
   return (
-    <main className={styles.container}>
-      <div className={styles.backButton}>
-        <ArrowLeft />
-        git
-      </div>
-      <AlinoLogo height="50px" />
-      <section className={styles.form}>
-        <div>
-          <h2 className={styles.title}>Iniciar sesion</h2>
-          <p className={styles.paraph}>Si no tienes cuenta, crea una</p>
-        </div>
-        <form className={styles.inputs}>
-          <div className={styles.inputContainer}>
-            <input type="text" placeholder="email" className={styles.input} />
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <Link href="/">
+          <div className={styles.backButton}>
+            <ArrowLeft />
           </div>
-          <div className={styles.inputContainer}>
-            <input
-              type="password"
-              placeholder="password"
-              className={styles.input}
-            />
+        </Link>
+        <AlinoLogo height="50px" />
+        <section className={styles.form}>
+          <div>
+            <h2 className={styles.title}>Iniciar sesion</h2>
+            <p className={styles.paraph}>Si no tienes cuenta, crea una</p>
           </div>
-          <button className={styles.buttom}>Iniciar sesion</button>
-        </form>
-        <div className={styles.moreInfo}>
-          <p>¿Olvidaste tu constraseña?</p>
-          <p>¿No tienes una cuenta?</p>
-        </div>
-      </section>
-    </main>
+          <form className={styles.inputs}>
+            <div className={styles.inputContainer}>
+              <input type="text" placeholder="email" className={styles.input} />
+            </div>
+            <div className={styles.inputContainer}>
+              <input
+                type="password"
+                placeholder="password"
+                className={styles.input}
+              />
+            </div>
+            <button className={styles.buttom}>Iniciar sesion</button>
+          </form>
+          <div className={styles.moreInfo}>
+            <p>¿Olvidaste tu constraseña?</p>
+            <p>¿No tienes una cuenta?</p>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
