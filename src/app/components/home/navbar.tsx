99939c8c36707d8498684e8b40d0d01439aeef23
Agustin Manelli <agustinmanelli@hotmail.com>
2024-04-09 13:26:16 -0300

    add max width in landing and navbar

--- src/app/components/home/navbar.tsx
+++ src/app/components/home/navbar.tsx
@@ -5,16 +5,18 @@ import styles from "./navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
-      <AlinoLogo height="35px" />
-      <ButtonComponent
-        name="Iniciar sesion"
-        back="rgb(250, 250, 250)"
-        hover="rgb(240, 240, 240)"
-        letterColor="#000"
-        to="login"
-        strokeB={false}
-      />
-    </nav>
+    <div className={styles.container}>
+      <nav className={styles.navbar}>
+        <AlinoLogo height="35px" />
+        <ButtonComponent
+          name="Iniciar sesion"
+          back="rgb(250, 250, 250)"
+          hover="rgb(240, 240, 240)"
+          letterColor="#000"
+          to="login"
+          strokeB={false}
+        />
+      </nav>
+    </div>
   );
 }
