0e1ac046fe162411b1c215146c40c1075d2c4fe9
Agustin Manelli <agustinmanelli@hotmail.com>
2024-04-09 18:17:32 -0300

    fix update auth

--- src/app/utils/supabase/server.ts
+++ src/app/utils/supabase/server.ts
@@ -1,7 +1,7 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export default async function createClient() {
+export function createClient() {
   const cookieStore = cookies();
 
   return createServerClient(
