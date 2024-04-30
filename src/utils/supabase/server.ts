a00045ef2775a8697ef3a7e8c5f76edde769c215
Agustin Manelli <agustinmanelli@hotmail.com>
2024-04-30 10:23:17 -0300

    delete function in server.ts

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -35,16 +35,3 @@ export function createClient() {
     }
   );
 }
-
-export const getSession = cache(async () => {
-  const supabase = createClient();
-  try {
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    return user;
-  } catch (error) {
-    console.error("Error:", error);
-    return null;
-  }
-});
