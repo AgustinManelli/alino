dfd03977ba23f7a682e605e2b9f27f859130f24b
Agustin Manelli <agustinmanelli@hotmail.com>
2024-05-07 23:36:37 -0300

    fix error

--- src/app/alino-app/components/todo/todo-tasks-section.tsx
+++ src/app/alino-app/components/todo/todo-tasks-section.tsx
@@ -1,18 +1,12 @@
 "use client";
 
 import styles from "./todo-tasks-section.module.css";
-import { useEffect } from "react";
-import { useLists } from "@/store/lists";
-import { GetTasks } from "@/lib/todo/actions";
 import { Database } from "@/lib/todosSchema";
 import TodoCard from "./todo-card";
 
 type ListsType = Database["public"]["Tables"]["todos_data"]["Row"];
 
 export default function TodoTasksSection({ setList }: { setList: ListsType }) {
-  const setLists = useLists((state) => state.setLists);
-  const lists = useLists((state) => state.lists);
-
   return (
     <div className={styles.container}>
       {setList?.tasks?.map((task) => <TodoCard task={task} />)}
