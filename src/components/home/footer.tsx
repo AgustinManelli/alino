90b69f45bf7ba0f94e3278dd25740fa6a846588d
Agustin Manelli <agustinmanelli@hotmail.com>
2024-08-19 18:03:34 -0300

    add footer stripe

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -2,11 +2,19 @@ import styles from "./footer.module.css";
 import logo from "../../../public/alinologo.webp";
 
 export default function Footer() {
+  var year = new Date().getFullYear();
   return (
     <div className={styles.footerContainer}>
       <div className={styles.footerContent}>
         <img src={logo.src} className={styles.img} />
         {/*<section>footer content section</section>*/}
+      </div>
+      <div className={styles.footerStripeContainer}>
+        <div className={styles.footerStripe}>
+          <p>
+            alino - <span>{year}</span>
+          </p>
+        </div>
       </div>
     </div>
   );
