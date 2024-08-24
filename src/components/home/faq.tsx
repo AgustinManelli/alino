001d932f4ec7ba7a3d33e7b5c13f9bc34cfc0db8
Agustin Manelli <agustinmanelli@hotmail.com>
2024-08-24 12:04:13 -0300

    update accordion texts

--- src/components/home/faq.tsx
+++ src/components/home/faq.tsx
@@ -11,23 +11,22 @@ export default function Faq() {
           items={[
             {
               title: "¿Es completamente gratis?",
-              content:
-                "Si, alino es completamente gratis, aunque, aún está en fase de desarrollo.",
+              content: "Si, Alino es completamente gratis.",
             },
             {
               title: "¿De que se trata alino?",
               content:
-                "Alino, como proyecto, es una aplicación donde podrás guardar tareas, estas distribuídas en distintas listas generales de tareas, la idea principal es organizar trabajos, tareas y calendarios, es decir, es una aplicación orientada a organizar tareas del tipo TO-DO app. Aún se encuentra en una fase muy temprana de su desarrollo.",
+                "Alino, como proyecto, es una aplicación donde podrás guardar tareas, estas distribuídas en distintas listas, es decir, es una aplicación orientada a organizar tu trabajo del tipo TO-DO app. Aún se encuentra en una fase muy temprana de su desarrollo.",
             },
             {
               title: "¿Aún se encuentra en fase de desarrollo?",
               content:
-                "Sí, si bien, puede probar la aplicación, aún se encuentra en una fase muy temprana de su desarrollo. Estamos convencidos que es un buen proyecto y esperamos dejarlo completamente funcional lo antes posible.",
+                "Sí, si bien puedes probar la aplicación, aún se encuentra en una fase muy temprana de su desarrollo. Estamos convencidos que es un buen proyecto y esperamos dejarlo completamente funcional lo antes posible.",
             },
             {
-              title: "¿Puedo enviad feedback sobre bugs o cambios?",
+              title: "¿Puedo enviar feedback sobre bugs o cambios?",
               content:
-                "Estaríamos muy agradecidos si nos enviaras feedback sobre nuestra app, no solo nos ayudaría en cuanto a su funcionamiento si no también a conocer las necesidades de los usuarios. <br />Puedes dejarnos un mensaje en <u>alino.cba@gmail.com</u>",
+                "Estaríamos muy agradecidos si nos envías feedback sobre nuestra app, no solo nos ayudaría en cuanto a su funcionamiento si no también a conocer las necesidades de los usuarios. <br />Puedes dejarnos un mensaje en <u>alino.cba@gmail.com</u>",
             },
           ]}
         />
