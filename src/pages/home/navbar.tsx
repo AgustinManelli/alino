f25dd5ccd9b20ebafc68fd605336bcc00943891f
Agustin Manelli <agustinmanelli@hotmail.com>
2024-12-18 16:48:25 -0300

    add navbar button vip

--- src/pages/home/navbar.tsx
+++ src/pages/home/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AlinoLogo, UserIcon } from "../../lib/ui/icons";
+import { AlinoLogo, UserIcon, Crown } from "../../lib/ui/icons";
 import { ButtonComp } from "@/components";
 import styles from "./navbar.module.css";
 
@@ -10,6 +10,25 @@ export default function Navbar() {
       <nav className={styles.navbar}>
         <AlinoLogo style={{ height: "35px" }} />
         <div className={styles.buttonsDiv}>
+          <ButtonComp
+            text=""
+            background="rgb(240, 240, 240)"
+            hover="rgb(230, 230, 230)"
+            letterColor="#000"
+            to="404"
+            strokeBorder={false}
+            withLoader={true}
+            style={{ pointerEvents: "none", opacity: "0.4" }}
+          >
+            <Crown
+              style={{
+                strokeWidth: "1.5",
+                stroke: "#1c1c1c",
+                width: "20px",
+                height: "auto",
+              }}
+            />
+          </ButtonComp>
           <ButtonComp
             text="iniciar sesión"
             background="rgb(240, 240, 240)"
