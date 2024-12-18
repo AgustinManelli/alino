7bdf1dddaefea365353932ed5f4feefa12b90777
Agustin Manelli <agustinmanelli@hotmail.com>
2024-12-18 15:17:06 -0300

    landing changes

--- src/pages/home/header.tsx
+++ src/pages/home/header.tsx
@@ -8,35 +8,37 @@ import { useEffect } from "react";
 
 export default function Header() {
   const setLoading = useLoaderStore((state) => state.setLoading);
+
   useEffect(() => {
     document.body.style.overflow = "";
     setLoading(false);
-  }, []);
+  }, [setLoading]);
 
   return (
-    <div className={styles.container}>
-      <header className={styles.hero}>
-        <section className={styles.heroContainer}>
-          <div className={styles.ctaContainer}>
-            <h1 className={styles.h1Text}>Tu organizador en linea</h1>
-            <p className={styles.paraph}>
-              Organizá tus clases, tareas o trabajos en un mismo lugar. <br />
-              Es <span className={styles.paraphSpan}>100% gratis.</span>
+    <div className={styles.headerWrapper}>
+      <header className={styles.header}>
+        <section className={styles.content}>
+          <div className={styles.textContainer}>
+            <h1 className={styles.title}>Tu organizador en línea</h1>
+            <p className={styles.subtitle}>
+              Organizá tus clases, tareas o trabajos
+              <span className={styles.highlight}> en un mismo lugar.</span>
             </p>
             <ButtonComp
-              text="Pruebalo ya mismo"
+              text="Pruébalo ya mismo"
               background="#1c1c1c"
               hover="rgb(230, 230, 230)"
               letterColor="#fff"
               to="sign-in"
               strokeBorder={true}
               withLoader={true}
+              aria-label="Prueba Alino"
             />
           </div>
           <img
             src={imgHero.src}
-            className={styles.img}
-            alt="alino logo"
+            className={styles.image}
+            alt="Vista previa de Alino"
             loading="lazy"
           />
         </section>
