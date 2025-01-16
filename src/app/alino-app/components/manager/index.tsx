f2ca0c31c83b94917a3cd3193ead70951e3f0874
Agustín Manelli <agustinmanelli@hotmail.com>
2025-01-16 17:25:36 -0300

    fix error

--- src/app/alino-app/components/manager/index.tsx
+++ src/app/alino-app/components/manager/index.tsx
@@ -21,7 +21,8 @@ export default function Manager({
   const setBlurredFx = useBlurredFxStore((state) => state.setColor);
 
   useEffect(() => {
-    if (setList?.color !== undefined) setBlurredFx(setList?.color);
+    if (setList?.color !== undefined && setList?.color !== null)
+      setBlurredFx(setList?.color);
   });
 
   const temporalHomeListLength = lists.reduce(
