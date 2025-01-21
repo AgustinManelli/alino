ef2ff45b448c15f8c9f4b7116083050ccfe572db
Agustín Manelli <agustinmanelli@hotmail.com>
2025-01-21 00:51:36 -0300

    test

--- src/app/alino-app/components/blurredFx/index.tsx
+++ src/app/alino-app/components/blurredFx/index.tsx
@@ -1,24 +1,16 @@
 "use client";
 
-import useMobileStore from "@/store/useMobileStore";
 import styles from "./blurredFx.module.css";
 import { useBlurredFxStore } from "@/store/useBlurredFx";
 
 export default function BlurredFx() {
   const blurredFx = useBlurredFxStore((state) => state.color);
-  const isMobile = useMobileStore();
   return (
-    <>
-      {!isMobile ||
-        (isMobile &&
-          !window.matchMedia("(display-mode: standalone)").matches && (
-            <div
-              className={styles.blurred}
-              style={{
-                boxShadow: `${blurredFx} 20px 200px 240px`,
-              }}
-            ></div>
-          ))}
-    </>
+    <div
+      className={styles.blurred}
+      style={{
+        boxShadow: `${blurredFx} 20px 200px 240px`,
+      }}
+    ></div>
   );
 }
