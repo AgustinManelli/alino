44da418c489a06d795af5c0457e0bbf08f1d8f5e
Agustín Manelli <agustinmanelli@hotmail.com>
2025-01-24 15:32:51 -0300

    add popup oauth login

---
+++ src/app/auth/popup-callback/page.ts
@@ -0,0 +1,33 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const PopupCallback = () => {
+  const [mounted, setMounted] = useState(false);
+  const params = useSearchParams();
+  const code = params?.get("code");
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (!code) return;
+    // Send the code to the parent window
+    const channel = new BroadcastChannel("popup-channel");
+    channel.postMessage({ authResultCode: code });
+    window.close();
+  }, []);
+
+  if (!mounted) return null;
+
+  // Close the popup if there is no code
+  if (!code) {
+    window.close();
+  }
+
+  return null;
+};
+
+export default PopupCallback;
