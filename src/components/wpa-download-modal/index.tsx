7fd967bd37ed75993989b0df7a025653add27ea1
Agustín Manelli <agustinmanelli@hotmail.com>
2025-01-25 01:07:15 -0300

    test

--- src/components/wpa-download-modal/index.tsx
+++ src/components/wpa-download-modal/index.tsx
@@ -8,48 +8,49 @@ import useMobileStore from "@/store/useMobileStore";
 import { Modal } from "./modal";
 
 export function WpaDownloadModal() {
-  // const storedIsOpen = localStorage.getItem("pwa-user-md");
-  // const [isOpen, setIsOpen] = useState(
-  //   storedIsOpen ? JSON.parse(storedIsOpen) : false
-  // );
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const storedIsOpen = localStorage.getItem("pwa-user-md");
+  const [isOpen, setIsOpen] = useState(
+    storedIsOpen ? JSON.parse(storedIsOpen) : false
+  );
   const { isMobile, setIsMobile } = useMobileStore();
 
   const handleCloseModal = (value: boolean) => {
     setIsOpen(value);
-    // localStorage.setItem("pwa-user-md", "false");
+    localStorage.setItem("pwa-user-md", "false");
   };
 
   useEffect(() => {
-    // if (storedIsOpen === null) {
-    const timer = setTimeout(() => {
-      setIsOpen(true);
-    }, 5000);
+    if (storedIsOpen === null) {
+      const timer = setTimeout(() => {
+        setIsOpen(true);
+      }, 5000);
 
-    return () => clearTimeout(timer);
-    // }
-  }, []);
+      return () => clearTimeout(timer);
+    }
+  }, [storedIsOpen]);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 850);
-    };
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setIsMobile(window.innerWidth < 850);
+  //   };
 
-    // Inicializar con el valor actual
-    handleResize();
+  //   // Inicializar con el valor actual
+  //   handleResize();
 
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  //   window.addEventListener("resize", handleResize);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
 
-  // const isStandalone = window.matchMedia("(display-mode: standalone)").matches;
+  const isStandalone = window.matchMedia("(display-mode: standalone)").matches;
 
   return (
     <>
       <AnimatePresence>
-        {isOpen && isMobile && <Modal setIsOpen={handleCloseModal} />}
+        {isOpen && isMobile && !isStandalone && (
+          <Modal setIsOpen={handleCloseModal} />
+        )}
       </AnimatePresence>
     </>
   );
