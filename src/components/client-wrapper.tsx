2b7d7a69d3faff2c7192f19a33cf2ab5e50d0cf1
Agustín Manelli <agustinmanelli@hotmail.com>
2025-01-31 20:24:29 -0300

    fix error

--- src/components/client-wrapper.tsx
+++ src/components/client-wrapper.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import { useEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useLoaderStore } from "@/store/useLoaderStore";
 
 export const useNavigation = () => {
   const { setLoading } = useLoaderStore();
   const pathname = usePathname();
-  const searchParams = useSearchParams();
+  // const searchParams = useSearchParams();
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 500);
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return { setLoading };
 };
