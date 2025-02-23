30f8caa207d872fbdf07bebcfbc13c4eb0184ff0
Agustin Manelli <agustinmanelli@hotmail.com>
2025-02-23 15:14:51 -0300

    delete landingpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,14 @@
-import Navbar from "@/components/pages/home/navbar";
-import { Header } from "@/components/pages/home/header";
-import Footer from "@/components/pages/home/footer";
-import Faq from "@/components/pages/home/faq";
-import { Cards } from "@/components/pages/home/cards";
-import Image from "next/image";
-import navbar_blur from "../../public/navbar_blur.webp";
-// import { AppInformation } from "@/components/pages/home/app-information";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main style={{ overflow: "hidden", position: "relative" }}>
-      <Image
-        src={navbar_blur}
-        alt=""
-        priority
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          minWidth: "720px",
-          height: "auto",
-          opacity: 0.5,
-        }}
-      />
-      <Navbar />
-      <Header />
-      <Cards />
-      {/* <AppInformation /> */}
-      <Faq />
-      <Footer />
-    </main>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/sign-in");
+  }, [router]);
+
+  return null; // No renderiza nada en esta página
 }
