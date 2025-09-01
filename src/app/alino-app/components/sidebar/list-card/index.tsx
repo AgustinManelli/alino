9204fbc8b2a97c9ae430055b5a1c2ebe2a4e4817
Agustin Manelli <agustinmanelli@hotmail.com>
2025-09-01 18:43:03 -0300

    fix(sidebar): fix final listitems and folderitems on dragEnd

--- src/app/alino-app/components/sidebar/list-card/index.tsx
+++ src/app/alino-app/components/sidebar/list-card/index.tsx
@@ -69,13 +69,11 @@ export const ListCard = memo(({ list, inFolder = false }: props) => {
 
   //funciones
   const handleLeave = useCallback(() => {
-    // if (pathname === `/alino-app/${list.list_id}`) router.replace("/alino-app");
     if (!list) return;
     leaveList(list.list_id);
   }, [list, leaveList, pathname]);
 
   const handleDelete = useCallback(() => {
-    // if (pathname === `/alino-app/${list.list_id}`) router.replace("/alino-app");
     if (!list) return;
     deleteList(list.list_id);
   }, [list, deleteList, pathname]);
