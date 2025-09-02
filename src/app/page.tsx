abaaaa9b19f67fea07ee06f2c9d6e5162da049bf
Agustin Manelli <agustinmanelli@hotmail.com>
2025-09-01 22:55:05 -0300

    refactor(app): refactor layouts components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/sign-in");
-  }, [router]);
-
-  return null;
+  return redirect("/sign-in");
 }
