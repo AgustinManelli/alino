e1ab581670e5a56fa81ca3f099e2431d1acbcd85
Agustin Manelli <agustinmanelli@hotmail.com>
2025-09-04 21:54:02 -0300

    perf(navbar-button): delete motion and animate with css

--- src/app/alino-app/components/sidebar/navbar/navbar-button.tsx
+++ src/app/alino-app/components/sidebar/navbar/navbar-button.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { motion } from "motion/react";
-
 import { MenuIcon } from "@/components/ui/icons/icons";
 import styles from "./navbar.module.css";
 
@@ -12,26 +10,10 @@ interface Props {
 
 export const NavbarButton = ({ navbarOpened, toggleNavbar }: Props) => {
   return (
-    <motion.button
+    <button
       className={styles.mobileButton}
-      initial={{ scale: 0, opacity: 0 }}
-      animate={{
-        opacity: 1,
-        scale: navbarOpened ? 0 : 1,
-        transition: {
-          opacity: { duration: 0.2 },
-          scale: { duration: 0.2 },
-        },
-      }}
-      exit={{
-        opacity: 0,
-        scale: 0,
-        transition: {
-          opacity: { duration: 0.2 },
-          scale: { duration: 0.2 },
-        },
-      }}
       onClick={toggleNavbar}
+      style={{ opacity: navbarOpened ? 0 : 1, scale: navbarOpened ? 0 : 1 }}
     >
       <MenuIcon
         style={{
@@ -41,6 +23,6 @@ export const NavbarButton = ({ navbarOpened, toggleNavbar }: Props) => {
           strokeWidth: "2",
         }}
       />
-    </motion.button>
+    </button>
   );
 };
