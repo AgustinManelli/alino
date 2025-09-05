9e60519dc282183bd389483345b0fd72fe27ad97
Agustin Manelli <agustinmanelli@hotmail.com>
2025-09-05 16:19:45 -0300

    fix: folder case-sensitive

---
+++ src/app/alino-app/components/sidebar/Navbar/NavbarButton.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { MenuIcon } from "@/components/ui/icons/icons";
+import styles from "./navbar.module.css";
+
+interface Props {
+  navbarOpened: boolean;
+  toggleNavbar: () => void;
+}
+
+export const NavbarButton = ({ navbarOpened, toggleNavbar }: Props) => {
+  return (
+    <button
+      className={styles.mobileButton}
+      onClick={toggleNavbar}
+      style={{ opacity: navbarOpened ? 0 : 1, scale: navbarOpened ? 0 : 1 }}
+    >
+      <MenuIcon
+        style={{
+          width: "25px",
+          height: "auto",
+          stroke: "var(--text)",
+          strokeWidth: "2",
+        }}
+      />
+    </button>
+  );
+};
