8c04d4a60f5c0ec8803bea2786b79b37e99c5ffc
Agustin Manelli <agustinmanelli@hotmail.com>
2025-09-09 17:20:30 -0300

    fix: fix trigger route.ts mailto

--- src/app/api/push/trigger/route.ts
+++ src/app/api/push/trigger/route.ts
@@ -6,7 +6,7 @@ const vapidPublicKey = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!;
 const vapidPrivateKey = process.env.VAPID_PRIVATE_KEY!;
 
 webpush.setVapidDetails(
-  "mailto:ayuda@alino.online.",
+  "mailto:ayuda@alino.online",
   vapidPublicKey,
   vapidPrivateKey
 );
